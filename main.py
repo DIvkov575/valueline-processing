95054f3a9a148476c6efecc4c6847f801fdbc63f
Divkov575 <dmitriy@ivkov.net>
2023-08-30 20:27:25 -0400

    v1 - functional

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
-"""
-## USAGE
-```
-$ python pdf_contrast.py 2.3 -i in.pdf -o out.pdf
-Loading pdf in.pdf
-Pages: 48
-Contrast x2.3: 100%|███████████████████████| 48/48 [00:02<00:00, 18.42pages/s]
-Saving pdf to out.pdf
-```
-"""
 import io
-from PIL import ImageEnhance
+import os
+from time import perf_counter
+from multiprocessing import Pool
+
 import img2pdf
 import pdf2image
+from PIL import ImageEnhance, Image
 from tqdm import tqdm
 
 contrast: float = 1.6
 brightness: float = 0.75
 
 
-def pdf_contrast(input_file: str, output_file: str):
+def process(file_name: str) -> None:
     """
-    Create a new pdf corresponding to the contrast multiplier
-    `input_file`: name the of the input_file
-    `contrast`: contrast multiplier. 1 corresponds to no change
-    `output_file`: name of the file to be saved
+    Adjusts both brightness and contrast
+    'input' is prepended to filename for ingesting files
+    'output' is prepended to filename for writing files
     """
 
-    # print(f'Loading pdf {input_file}')
-    input_images = pdf2image.convert_from_path(input_file)
-    # print(f'Pages: {len(input_images)}')
+    input_path: str = os.path.join("input", file_name)  # input file name 🤓
+    output_path: str = os.path.join("output", file_name)  # output file name 🤓
+    output_images: list[bytes] = []  # used for constructing output pdf
 
-    output_images: list[bytes] = []
-    for raw_img in tqdm(input_images,
-                    desc=f"Contrast x{contrast}",
-                    unit="pages"
-                    ):
-        im_1 = ImageEnhance.Brightness(raw_img).enhance(brightness)
-        out_im = ImageEnhance.Contrast(im_1).enhance(contrast)
+    for raw_img in tqdm(pdf2image.convert_from_path(input_path), desc=f"{file_name}", unit="page"):
+        im_1: Image = ImageEnhance.Brightness(raw_img).enhance(brightness)
+        out_im: Image = ImageEnhance.Contrast(im_1).enhance(contrast)
+        out_img_bytes: io.BytesIO = io.BytesIO()
 
-        out_img_bytes = io.BytesIO()
         out_im.save(out_img_bytes, format="JPEG")
         output_images.append(out_img_bytes.getvalue())
 
-    # print(f'Saving pdf to {output_file}')
-    with open(output_file, "wb") as outf:
+    with open(output_path, "wb") as outf:
         img2pdf.convert(*output_images, outputstream=outf)
 
 
 if __name__ == "__main__":
-    pdf_contrast("0.pdf", "0-1.pdf")
+    if not os.path.exists("input/"):
+        raise Exception("Please place input files in 'input/'")
+    if not os.path.exists("output/"):
+        os.mkdir("output")
+
+    start_time: float = perf_counter()  # for timing execution
+    with Pool(processes=4) as pool:
+        pool.map(process, os.listdir("input"))
+    print(f"completed in: {perf_counter() - start_time}")  # for timing execution
+
+    start_time: float = perf_counter()  # for timing execution
+    for file in os.listdir("input"): process(file)
+    print(f"completed in: {perf_counter() - start_time}")  # for timing execution

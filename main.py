7bed0d520459a8262af9c2b2c39108ae39db7991
Divkov575 <dmitriy@ivkov.net>
2023-09-20 17:48:35 -0400

    update requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# from src.xlsx.decimal_correction import correct_decimals
+from src.xlsx.decimal_correction import correct_decimals_all
 # from src.pdf.contrast_brightness import adjust_bc
 
 if __name__ == '__main__':
-    correct_decimals()
+    correct_decimals_all()
 
     
